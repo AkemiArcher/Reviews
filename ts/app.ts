2143fbe256130a6b16633b6f0504c6d2bc773e79
akemi <sergeytolkahev360@gmail.com>
2026-08-30 17:52:20 +0300

    added local images

--- ts/app.ts
+++ ts/app.ts
@@ -5,28 +5,28 @@ const reviews = [
     id: 1,
     name: "susan smith",
     job: "web developer",
-    img: "https://www.course-api.com/images/people/person-1.jpeg",
+    img: "assets/person-1.jpeg",
     text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
   },
   {
     id: 2,
     name: "anna johnson",
     job: "web designer",
-    img: "https://www.course-api.com/images/people/person-2.jpeg",
+    img: "assets/person-2.jpeg",
     text: "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
   },
   {
     id: 3,
     name: "peter jones",
     job: "intern",
-    img: "https://www.course-api.com/images/people/person-4.jpeg",
+    img: "assets/person-4.jpeg",
     text: "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
   },
   {
     id: 4,
     name: "bill anderson",
     job: "the boss",
-    img: "https://www.course-api.com/images/people/person-3.jpeg",
+    img: "assets/person-3.jpeg",
     text: "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
@@ -53,12 +53,11 @@ function showPerson(person: number) {
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  
   showPerson(currentItem);
 });
 
 nextBtn?.addEventListener("click", () => {
-  if (currentItem >= reviews.length-1) {
+  if (currentItem >= reviews.length - 1) {
     currentItem = 0;
   }
   showPerson(currentItem++);
@@ -66,12 +65,12 @@ nextBtn?.addEventListener("click", () => {
 
 prevBtn?.addEventListener("click", () => {
   if (currentItem < 0) {
-    currentItem = reviews.length-1;
+    currentItem = reviews.length - 1;
   }
   showPerson(currentItem--);
 });
 
 randomBtn?.addEventListener("click", () => {
-	currentItem = Math.floor(Math.random()*reviews.length);
-	showPerson(currentItem);
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
 });
